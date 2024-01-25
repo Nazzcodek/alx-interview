cd6dd6f80123b4f08888ea24f9f2693efb8a728c
Nazzcodek <bellnas09@gmail.com>
2024-01-25 22:55:16 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-This module reads from stdin, computes metrics, and handles keyboard interruptions.
-It calculates the total file size and counts the number of occurrences of each status code.
+This module reads from stdin, computes metrics,
+and handles keyboard interruptions.
+It calculates the total file size and
+counts the number of occurrences of each status code.
 """
 
 import sys
 import signal
 import re
 from collections import defaultdict
+
 
 def signal_handler(sig, frame):
     """
@@ -16,14 +19,17 @@ def signal_handler(sig, frame):
     print_stats()
     sys.exit(0)
 
+
 def print_stats():
     """
-    Prints the total file size and the number of occurrences of each status code.
+    Prints the total file size and
+    the number of occurrences of each status code.
     The status codes are printed in ascending order.
     """
     print(f"File size: {total_size}")
     for code in sorted(status_codes.keys()):
         print(f"{code}: {status_codes[code]}")
+
 
 total_size = 0
 status_codes = defaultdict(int)
@@ -31,12 +37,14 @@ line_count = 0
 
 signal.signal(signal.SIGINT, signal_handler)
 
-pattern = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'
+pattern = (r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '
+           r'"GET /projects/260 HTTP/1.1" (\d{3}) (\d+)')
 
 for line in sys.stdin:
     """
     For each line in stdin, it checks if the line matches the pattern.
-    If it does, it increments the count for the status code and adds the file size to the total size.
+    If it does, it increments the count for the status code
+    and adds the file size to the total size.
     After every 10 lines, it prints the statistics.
     """
     match = re.search(pattern, line)

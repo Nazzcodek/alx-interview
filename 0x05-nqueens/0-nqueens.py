635b00eb70f4167546abe441962d145fdc722b05
Nazzcodek <bellnas09@gmail.com>
2024-02-11 08:10:54 +0000

    solved n queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" this is the n-queen chess solution module"""
+import sys
+
+
+def is_safe(board, row, col, n):
+    """Check if a queen can be placed at board[row][col]."""
+    # Check column leftwards
+    if any(board[row][i] == 1 for i in range(col)):
+        return False
+    # Check upper diagonal leftwards
+    if any(board[i][j] == 1 for i, j in zip(range(row, -1, -1),
+                                            range(col, -1, -1))):
+        return False
+    # Check lower diagonal leftwards
+    if any(board[i][j] == 1 for i, j in zip(range(row, n,  1),
+                                            range(col, -1, -1))):
+        return False
+    return True
+
+
+def solve_nq_util(board, col, n, solutions):
+    """Recursively solve the N-Queens problem."""
+    if col == n:
+        rec = [(i, j) for i in range(n) for j in range(n) if board[i][j] == 1]
+        solutions.append(rec)
+        return True
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            if solve_nq_util(board, col + 1, n, solutions):
+                return True
+            board[i][col] = 0
+    return False
+
+
+def solve_nq(n):
+    """Solve the N-Queens problem for a given size."""
+    if not isinstance(n, int):
+        raise ValueError("N must be an integer.")
+    if n < 4:
+        raise ValueError("N must be at least  4.")
+    board = [[0]*n for _ in range(n)]
+    solutions = []
+    solve_nq_util(board, 0, n, solutions)
+    return solutions
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise SystemExit("Usage: python script.py N")
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        raise SystemExit("N must be an integer.")
+    solutions = solve_nq(n)
+    for solution in solutions:
+        print(solution)

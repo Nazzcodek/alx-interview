3b3c74de487fa39b4c6dc62e6f0a5104e642d5f0
Nazzcodek <bellnas09@gmail.com>
2024-05-09 06:37:08 +0000

    perimeter solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""this is the island perimeter module"""
+
+def island_perimeter(grid):
+    """Calculate the perimeter of an island."""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                # Check the cells around the current cell
+                if i == 0 or grid[i - 1][j] == 0:  # Top cell
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:  # Bottom cell
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Left cell
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:  # Right cell
+                    perimeter += 1
+    return perimeter
+
